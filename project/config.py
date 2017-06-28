5a4c6cc33d98ad817967812519db93430420e689
stevostean <se@studiomzk.com>
2017-06-29 00:29:11 +0200

    Part One: postgresql setup & env variables.

--- project/config.py
+++ project/config.py
@@ -1,23 +1,30 @@
 # project/config.py
 
 
+import os
+
+
 class BaseConfig:
     """Base configuration class"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevConfig(BaseConfig):
     """Development configuration class"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration class"""
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProdConfig(BaseConfig):
     """Production configuration class"""
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
