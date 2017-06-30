548ff9c4cbf8dd97ff4d83dd42dd28d0243db230
stevostean <se@studiomzk.com>
2017-06-30 22:22:52 +0200

    Part One: Restful Routes completed, with seed_db command.

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@
 import unittest
 
 from flask_script import Manager
+from faker import Faker
 
 #from project import app, db
 from project import create_app, db
@@ -12,6 +13,7 @@ from project.api.models import User
 
 app = create_app()
 manager = Manager(app)
+fake = Faker()
 
 
 @manager.command
@@ -29,6 +31,17 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+@manager.command
+def seed_db():
+    """Seed the db with fake infos."""
+    for _ in range(10):
+        name = fake.first_name()
+        email = f"{name}@{fake.free_email_domain()}"
+        db.session.add(User(username=name, email=email))
+
+    db.session.commit()
+    print("Seeding db with fake data: done.")
 
 
 if __name__ == '__main__':
